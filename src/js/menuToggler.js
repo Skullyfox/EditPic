1b1411833d6ac44ecfb37bd3136826d26e6f26af
Skullyfox <Skullyfox.yt@gmail.com>
2018-04-28 23:40:03 +0200

    UPDATE

--- src/js/menuToggler.js
+++ src/js/menuToggler.js
@@ -1,7 +1,30 @@
-const   toolpicker  = document.querySelector('.toolPicker'),
-        menuToggler = document.querySelector('.menuToggle');
+const   toolpicker      = document.querySelector('.toolPicker'),
+        toolpickerDiv   = document.querySelectorAll('.toolPicker div'),
+        menuToggler     = document.querySelector('.menuToggle');
 
 menuToggler.addEventListener('click', () => {
+
+    let menuTogglerStatutClass  = menuToggler.classList[1],
+        toolPickerStatutClass   = toolpicker.classList[1];
+
+    switch (menuTogglerStatutClass) {
+        case "menuTogglerOff":
+            toolpicker.classList.remove("menuOff");
+            menuToggler.classList.remove("menuTogglerOff");
+            toolpicker.classList.add("menuOn");
+            menuToggler.classList.add("menuTogglerOn");
+            break;
+
+        default:   
+            toolpicker.classList.remove("menuOn");
+            menuToggler.classList.remove("menuTogglerOn");
+            toolpicker.classList.add("menuOff");
+            menuToggler.classList.add("menuTogglerOff");
+            break;
+    }
+});
+
+toolpicker.addEventListener('click', () => {
 
     let menuTogglerStatutClass  = menuToggler.classList[1],
         toolPickerStatutClass   = toolpicker.classList[1];
