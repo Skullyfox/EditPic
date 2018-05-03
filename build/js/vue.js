abd64b0e2cd38f460d03f188994a0c4273347b84
Skullyfox <Skullyfox.yt@gmail.com>
2018-05-03 17:41:18 +0200

    new features

--- build/js/vue.js
+++ build/js/vue.js
@@ -1,3 +1,35 @@
+var app = new Vue({
+
+    el: '#app',
+
+    data: {
+        targetData: 'resizePic',
+        picture: '',
+    },
+
+    methods: {
+        setSeen: function(e) {
+            if(app.picture != ''){
+                drawPic();
+                app.targetData = e.target.dataset.setview;
+            }else{
+                app.targetData = e.target.dataset.setview;
+            }
+        },
+        upload: function() {
+            console.log("upload tried");
+            app.picture = new SimpleImage(img);
+        }
+    }
+});
+var img = document.querySelector('#uploadFile'),
+    canvas = document.querySelector('.canvasShowPic');
+
+function drawPic() {
+    var pictureWillDraw = app._data.picture;
+    pictureWillDraw.drawTo(canvas);
+}
+
 Vue.component(
     'changeafterdotpic', {
         template: `
@@ -52,34 +84,3 @@ Vue.component(
             </div>
         `
     })
-var app = new Vue({
-
-    el: '#app',
-
-    data: {
-        targetData: 'resizePic',
-        picture: '',
-    },
-
-    methods: {
-        setSeen: function(e) {
-            if(app.picture != ''){
-                drawPic();
-                app.targetData = e.target.dataset.setview;
-            }else{
-                app.targetData = e.target.dataset.setview;
-            }
-        },
-        upload: function() {
-            console.log("upload tried");
-            app.picture = new SimpleImage(img);
-        }
-    }
-});
-var img = document.querySelector('#uploadFile'),
-    canvas = document.querySelector('.canvasShowPic');
-
-function drawPic() {
-    var pictureWillDraw = app._data.picture;
-    pictureWillDraw.drawTo(canvas);
-}
